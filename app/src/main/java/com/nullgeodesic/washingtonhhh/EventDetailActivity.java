a096e547f4e7ca176f2436dd6483586ce7ce5250
Dave Garred <dave.garred@gmail.com>
2019-04-07 07:09:02 -0700

    add kennel logos, fix color things

--- app/src/main/java/com/nullgeodesic/washingtonhhh/EventDetailActivity.java
+++ app/src/main/java/com/nullgeodesic/washingtonhhh/EventDetailActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nullgeodesic.washingtonhhh.dto.HashEventDto;
@@ -21,18 +22,18 @@ public class EventDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_event_detail_toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.activity_event_detail_floating_action_button);
-
-
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.activity_event_detail_toolbar);
+        final ImageView kennelLogo = (ImageView) findViewById(R.id.activity_event_detail_kennel_logo);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.activity_event_detail_floating_action_button);
         final TextView eventDescription = (TextView) findViewById(R.id.content_event_detail_description);
+
+
+        setSupportActionBar(toolbar);
 
         int position = getIntent().getIntExtra("position", 0);
         final HashEventDto hashEvent = ContentHolder.ITEMS.get(position);
         final String mapLink = hashEvent.mapLink;
-        if (mapLink == null || mapLink == "") {
+        if (mapLink == null) {
             fab.hide();
         } else {
             fab.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +45,13 @@ public class EventDetailActivity extends AppCompatActivity {
                 }
             });
         }
+        final int kennelDrawableId = hashEvent.kennel.drawableId();
+        if(kennelDrawableId == 0) {
+            kennelLogo.setVisibility(View.INVISIBLE);
+        } else {
+            kennelLogo.setImageResource(kennelDrawableId);
+        }
+
         toolbar.setTitle(hashEvent.eventName);
 
         final boolean hasHtml = Pattern.compile("<[^>]*>").matcher(hashEvent.description).find();
--- app/src/main/java/com/nullgeodesic/washingtonhhh/dto/Kennel.java
+++ app/src/main/java/com/nullgeodesic/washingtonhhh/dto/Kennel.java
@@ -1,8 +1,37 @@
 package com.nullgeodesic.washingtonhhh.dto;
 
+import com.nullgeodesic.washingtonhhh.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Kennel {
+
+    private static final Map<String, Integer> KENNEL_LOGOS = new HashMap<>();
+
+    static {
+        KENNEL_LOGOS.put("SEATTLE", R.drawable.logo_seattle);
+        KENNEL_LOGOS.put("SEAMON", R.drawable.logo_seamon);
+        KENNEL_LOGOS.put("RAIN_CITY", R.drawable.logo_raincity);
+        KENNEL_LOGOS.put("HSWTF", R.drawable.logo_hswtf);
+        KENNEL_LOGOS.put("PUGET_SOUND", R.drawable.logo_puget);
+        KENNEL_LOGOS.put("NO_BALLS", R.drawable.logo_nbh3);
+        KENNEL_LOGOS.put("TACOMA", R.drawable.logo_tacoma);
+    }
 
     public String id;
     public String name;
     public String alternativeImage;
+
+    public boolean isUnknown() {
+        return "UNKNOWN".equals(this.id);
+    }
+
+    public int drawableId() {
+        final Integer drawableId = KENNEL_LOGOS.get(this.id);
+        if (drawableId == null) {
+            return 0;
+        }
+        return drawableId;
+    }
 }
